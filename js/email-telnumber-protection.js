45c89bbc8056b2afe469a8657fbe4121f00e7c0b
OleDziewas <o.dziewas@t-online.de>
2025-02-02 22:01:35 +0100

    Add Datenschutz label and fix email visibility

---
+++ js/email-telnumber-protection.js
@@ -0,0 +1,3 @@
+const telefon = "87971672"
+const mail = "dziewas@"
+document.querySelectorAll(".telefon-email").forEach(element => element.innerHTML = `Telefon: +49176${telefon}<br/\>E-Mail: o.${mail}t-online.de`)
